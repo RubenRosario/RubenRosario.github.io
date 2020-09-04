8b60fc2c898a77cc8fc39a3c25d4e8abd1ec872b
RubenRosario_1101068 <1101068@isep.ipp.pt>
2020-09-04 14:41:41 -0400

    clean unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import Homepage from './pages/Homepage';
-import profileData from './profileData.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      landingData: profileData.landing,
-      experience: profileData.experience,
-      education: profileData.education,
-      skills: profileData.skills,
-      interests: profileData.interests,
-      awards: profileData.awards,
-    };
   }
+
   render() {
     return (
       <div className="App">
