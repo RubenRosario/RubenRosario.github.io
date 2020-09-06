d7673056bcae1558100aed922e1f8396824bd3a9
RubenRosario_1101068 <1101068@isep.ipp.pt>
2020-09-05 21:54:37 -0400

    rename component AboutMe

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import './AboutMe.css';
+
+const AboutMe = (props) => {
+  return <div> About me page is so hot!</div>;
+};
+
+export default AboutMe;
